8395c400deaec9a66147b0ef39ea34356c2d5a63
twisti <denizgebauer@web.de>
2025-12-28 14:29:59 +0100

    Change `AuditableLongIdTable` to extend `ULongIdTable` for improved ID handling

--- src/main/kotlin/dev/slne/surf/database/table/AuditableLongIdTable.kt
+++ src/main/kotlin/dev/slne/surf/database/table/AuditableLongIdTable.kt
@@ -1,10 +1,10 @@
 package dev.slne.surf.database.table
 
-import org.jetbrains.exposed.v1.core.dao.id.LongIdTable
+import org.jetbrains.exposed.v1.core.dao.id.ULongIdTable
 import org.jetbrains.exposed.v1.javatime.CurrentTimestamp
 import org.jetbrains.exposed.v1.javatime.timestamp
 
-open class AuditableLongIdTable(name: String = "") : LongIdTable(name) {
+open class AuditableLongIdTable(name: String = "") : ULongIdTable(name) {
     val createdAt = timestamp("created_at")
         .defaultExpression(CurrentTimestamp)
     val updatedAt = timestamp("updated_at")
